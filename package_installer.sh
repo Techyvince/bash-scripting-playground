55af45da9c03293439ad2d143a60206a82d4d6ea
Techyvince <ifezueprince@gmail.com>
2021-09-13 14:58:16 -0400

    added new lines

--- package_installer.sh
+++ package_installer.sh
@@ -1,4 +1,4 @@
-  GNU nano 2.9.8                 package_installer.sh                           
+  GNU nano 2.9.8                 package_installer.sh
 
 #!/bin/bash
 
@@ -12,10 +12,11 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/doc$
 sudo yum install docker-ce --nobest
 sudo yum install docker-ce --allowerasing -y
-sudo systemctl start docker 
+sudo systemctl start docker
 
 sudo yum install setools
 sudo yum install wget -y
 sudo yum install unzip
 sudo yum install tree
+sudo yum install neofetch
 sudo yum install neofetch
